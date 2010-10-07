becddc86d1a03b927997d58d7d6afb395c9d21e1
paulstuttard <paulstuttard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-07 12:41:43 +0000

    Add version 1.3 Tag
    git-svn-id: https://plugins.svn.wordpress.org/amazon-link/tags/1.3@297577 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- amazon.php
+++ amazon.php
@@ -4,7 +4,7 @@
 Plugin Name: Amazon Link
 Plugin URI: http://www.houseindorset.co.uk/plugins/amazon-link
 Description: Insert a link to Amazon using the passed ASIN number, with the required affiliate info.
-Version: 1.1
+Version: 1.3
 Text Domain: amazon-link
 Author: Paul Stuttard
 Author URI: http://www.houseindorset.co.uk
@@ -48,7 +48,15 @@ Layout:
 
 */
 
-require_once("aws_signed_request.php");
+require_once('aws_signed_request.php');
+
+require_once('include/displayForm.php');
+
+if (!class_exists('AmazonWishlist_ip2nation'))
+   include_once ( 'include/ip2nation.php');
+
+if( !class_exists( 'WP_Http' ) )
+    include_once( ABSPATH . WPINC. '/class-http.php' );
 
 if (!class_exists('AmazonWishlist_For_WordPress')) {
    class AmazonWishlist_For_WordPress {
@@ -56,14 +64,18 @@ if (!class_exists('AmazonWishlist_For_WordPress')) {
 /*****************************************************************************************/
       /// Settings:
 /*****************************************************************************************/
-      var $optionList = null;
+      var $optionList   = null;
+      var $country_data = null;
 
       // String to insert into Posts to indicate where to insert the amazon items
       var $TagHead    = '[amazon';
       var $TagTail    = ']';
 
+      var $optionVer  = 1;
       var $optionName = 'AmazonLinkOptions';
       var $Opts       = null;
+
+      var $multi_id   = 0;
 
       function AmazonWishlist_For_WordPress() {
          $this->__construct();
@@ -71,13 +83,25 @@ if (!class_exists('AmazonWishlist_For_WordPress')) {
 
       function __construct() {
          $this->URLRoot = plugins_url("", __FILE__);
-         $this->plugin_dir = dirname( plugin_basename( __FILE__ ) );
+         $this->base_name  = plugin_basename( __FILE__ );
+         $this->plugin_dir = dirname( $this->base_name );
+         $this->form = new AmazonWishlist_Options;
+         $this->ip2n = new AmazonWishlist_ip2nation;
 
+         add_filter('plugin_row_meta', array($this, 'registerPluginLinks'),10,2);
          add_filter('the_content', array($this, 'contentFilter'));
          add_filter('the_posts', array($this, 'stylesNeeded'));
          add_action('admin_menu', array($this, 'optionsMenu'));
          add_action('init', array($this, 'loadLang'));
+         register_activation_hook(__FILE__, array($this, 'activate'));
  
+      }
+
+      function registerPluginLinks($links, $file) {
+         if ($file == $this->base_name) {
+            $links[] = '<a href="options-general.php?page=' . $this->base_name .'">' . __('Settings','amazon-link') . '</a>';
+         }
+         return $links;
       }
 
       function optionsMenu() {
@@ -95,25 +119,76 @@ if (!class_exists('AmazonWishlist_For_WordPress')) {
          } else {
             $stylesheet = plugins_url("Amazon.css", __FILE__);
          }
+         $script = plugins_url("amazon.js", __FILE__);
 
          wp_enqueue_style('amazonlink-style', $stylesheet);
+         wp_enqueue_script('amazonlink-script', $script);
       }
 
       function loadLang() {
+
          /* load localisation  */
          load_plugin_textdomain('amazon-link', $this->plugin_dir . '/i18n', $this->plugin_dir . '/i18n');
 
-         /* Move Option List construction here so we can localise the strings */
+         /* Move Option List & Country Data construction here so we can localise the strings */
+         // Country specific aspects:
+         // full name of country,
+         // country flag image
+         // tld of main amazon site
+         // link to affiliate program site
+         // Default tag if none set up
+         $this->country_data = array('uk' => array('name' => __('United Kingdom', 'amazon-link'), 'flag' => 'images/flag_uk.gif', 'tld' => 'co.uk', 'site' => 'https://affiliate-program.amazon.co.uk', 'default_tag' => 'livpauls-21'),
+                                     'us' => array('name' => __('United States', 'amazon-link'), 'flag' => 'images/flag_us.gif', 'tld' => 'com', 'site' => 'https://affiliate-program.amazon.com', 'default_tag' => 'lipawe-20'),
+                                     'de' => array('name' => __('Germany', 'amazon-link'), 'flag' => 'images/flag_de.gif', 'tld' => 'de', 'site' => 'https://partnernet.amazon.de', 'default_tag' => 'lipas03-21'),
+                                     'fr' => array('name' => __('France', 'amazon-link'), 'flag' => 'images/flag_fr.gif', 'tld' => 'fr', 'site' => 'https://partenaires.amazon.fr', 'default_tag' => 'lipas03-21'),
+                                     'jp' => array('name' => __('Japan', 'amazon-link'), 'flag' => 'images/flag_jp.gif', 'tld' => 'jp', 'site' => 'https://affiliate.amazon.co.jp', 'default_tag' => 'lipawe-20'),
+                                     'ca' => array('name' => __('Canada', 'amazon-link'), 'flag' => 'images/flag_ca.gif', 'tld' => 'ca', 'site' => 'https://associates.amazon.ca', 'default_tag' => 'lipas-20'));
+
          $this->optionList = array(
+         'title' => array ( 'Type' => 'title', 'Value' => __('Amazon Link Plugin Options')),
+         'nonce' => array ( 'Type' => 'nonce', 'Name' => 'update-AmazonLink-options' ),
          'cat' => array ( 'Type' => 'hidden' ),
          'last' => array ( 'Type' => 'hidden' ),
          'asin' => array( 'Default' => '0', 'Type' => 'hidden'),
          'text' => array( 'Name' => __('Link Text', 'amazon-link'), 'Description' => __('Default text to display if none specified', 'amazon-link'), 'Default' => 'www.amazon.co.uk', 'Type' => 'text', 'Size' => '40'),
-         'tld' => array( 'Name' => __('Amazon Domain', 'amazon-link'), 'Description' => __('Which country\'s Amazon domain to use', 'amazon-link'), 'Default' => 'co.uk', 'Type' => 'selection', 'Options' => array ('co.uk', 'com', 'ca', 'de', 'jp', 'fr')),
-         'tag' => array( 'Name' => __('Affiliate Tag', 'amazon-link'), 'Description' => __('Amazon associates ID used to assign Amazon referral commissions', 'amazon-link'), 'Default' => 'livpauls-21', 'Type' => 'text'),
+         'localise' => array('Name' => __('Localise Amazon Link', 'wish-pics'), 'Description' => __('Make the link point to the users local Amazon website, (you must have ip2nation installed for this to work).', 'amazon-link'), 'Default' => '1', 'Type' => 'checkbox'),
+         'multi_cc' => array('Name' => __('Multinational Link', 'wish-pics'), 'Description' => __('Insert links to all other Amazon sites after primary link.', 'amazon-link'), 'Default' => '1', 'Type' => 'checkbox'),
+         'default_cc' => array( 'Name' => __('Default Country', 'amazon-link'), 'Description' => __('Which country\'s Amazon site to use by default', 'amazon-link'), 'Default' => 'uk', 'Type' => 'radio'),
          'pub_key' => array( 'Name' => __('AWS Public Key', 'amazon-link'), 'Description' => __('Public key provided by your AWS Account', 'amazon-link'), 'Default' => '', 'Type' => 'text', 'Size' => '40'),
-         'priv_key' => array( 'Name' => __('AWS Private key', 'amazon-link'), 'Description' => __('Private key provided by your AWS Account.', 'amazon-link'), 'Default' => "", 'Type' => 'text', 'Size' => '40'));
+         'priv_key' => array( 'Name' => __('AWS Private key', 'amazon-link'), 'Description' => __('Private key provided by your AWS Account.', 'amazon-link'), 'Default' => "", 'Type' => 'text', 'Size' => '40'),
+         'button' => array( 'Type' => 'buttons', 'Buttons' => array( __('Update Options', 'amazon-link' ) => array( 'Class' => 'button-primary', 'Action' => 'AmazonLinkAction'))));
 
+         // Populate Country related options
+         foreach ($this->country_data as $cc => $data) {
+            $this->optionList['default_cc']['Options'][$cc]['Name'] = '<img style="height:14px;" src="'. $this->URLRoot. '/'. $data['flag'] . '">';
+            $this->optionList['default_cc']['Options'][$cc]['Input'] = 'tag_' . $cc;
+            $this->optionList['tag_' . $cc]['Type'] = 'option';
+            $this->optionList['tag_' . $cc]['Name'] = $data['name'] . __('Affiliate Tag', 'amazon-link');
+            $this->optionList['tag_' . $cc]['Default'] = $data['default_tag'];
+            $this->optionList['tag_' . $cc]['Description'] = sprintf(__('Enter your affiliate tag for %1$s.', 'amazon-link'), '<a href="'. $data['site']. '">'.$data['name'].'</a>' );
+         }
+      }
+
+      function activate() {
+
+         // Options structure changed so need to add the 'version' option and upgrade as appropriate...
+         $Opts = $this->getOptions();
+
+         if (!isset($Opts['version'])) {
+
+            $cc_map = array('co.uk' => 'uk', 'com' => 'us', 'fr' => fr, 'de' => 'de', 'ca' => 'ca', 'jp' => 'jp');
+
+            // Move from version 1.2 to 1.3 of the plugin (Option Version Null => 1)
+            if (isset($Opts['tld'])) {
+               $cc = isset($cc_map[$Opts['tld']]) ? $cc_map[$Opts['tld']] : 'uk';
+               $Opts['default_cc'] = $cc;
+               if (isset($Opts['tag'])) $Opts['tag_' . $cc] = $Opts['tag'];
+            }
+            unset($Opts['tld']);
+            unset($Opts['tag']);
+            $Opts['version'] = 1;
+            $this->saveOptions($Opts);
+         }
       }
 
       function stylesNeeded($posts){
@@ -178,7 +253,54 @@ if (!class_exists('AmazonWishlist_For_WordPress')) {
       }
 
 /*****************************************************************************************/
+      /// Localise Link Facility
+/*****************************************************************************************/
+
+      function get_country() {
+
+        // Pretty arbitrary mapping of domains to Amazon sites, default to 'com' - the 'international' site.
+        $country_map = array('uk' => array('uk', 'ie', 'gi', 'gl', 'nl', 'vg', 'cy', 'gb'),
+                             'fr' => array('fr', 'be', 'bj', 'bf', 'bi', 'cm', 'cf', 'td', 'km', 'cg', 'dj', 'ga', 'gp',
+                                           'gf', 'gr', 'pf', 'tf', 'ht', 'ci', 'lu', 'mg', 'ml', 'mq', 'yt', 'mc', 'nc',
+                                           'ne', 're', 'sn', 'sc', 'tg', 'vu', 'wf', 'it', 'es'),
+                             'de' => array('de', 'at', 'ch', 'no', 'dn', 'li', 'sk'),
+                             'ca' => array('ca', 'pm'),
+                             'jp' => array('jp')
+                            );
+                          
+        $country = False;
+
+        if (!isset($this->country)) {
+
+           if ($this->Settings['localise']) {
+              $cc = $this->ip2n->get_cc();
+
+              $country = 'us';
+              foreach ($country_map as $key => $countries) {
+                 if (in_array($cc, $countries)) {
+                    $country = $key;
+                    continue;
+                 }
+              }
+           }
+
+           if ($country)
+              $this->country = $country;
+           else
+              $this->country = $this->Settings['default_cc'];
+         }	
+
+         return $this->country;
+      }
+
+/*****************************************************************************************/
       /// Searches through the_content for our 'Tag' and replaces it with the lists or links
+      /*
+       * Performs 3 functions:
+       *   1. Searches through the posts, and if multinational link required or a wishlist then bring in the styles.
+       *   2. Process the content and replace the shortcode with amazon links and wishlists
+       *   3. Search through the content and record any Amazon ASIN numbers ready to generate a wishlist.
+       */
 /*****************************************************************************************/
       function contentFilter($content, $doRecs=TRUE, $doLinks=TRUE) {
 
@@ -209,9 +331,10 @@ if (!class_exists('AmazonWishlist_For_WordPress')) {
                } else if ($doLinks) {
                   // Generate Amazon Link
                   $this->tags[] = $this->Settings['asin'];
-                  $output = $this->makeLink($this->Settings['text']);
+                  $output = $this->make_links($this->Settings['asin'], $this->Settings['text']);
                } else {
                   $this->tags[] = $this->Settings['asin'];
+                  if ($this->Settings['multi_cc']) $this->stylesNeeded = True;
                   $output = substr($content, $found, ($tagEnd - $found) + strlen($this->TagTail));
                }
 
@@ -235,20 +358,56 @@ if (!class_exists('AmazonWishlist_For_WordPress')) {
          include('include/showOptions.php');
       }
 
-      public function getURL()
+      public function getURL($asin)
       {
-         $text="http://www.amazon." . $this->Settings['tld']. "/gp/product/". $this->Settings['asin']. "?ie=UTF8&tag=" . $this->Settings['tag'] ."&linkCode=as2&camp=1634&creative=6738&creativeASIN=". $this->Settings['asin'];
+         $top_cc  = $this->get_country();
+         $tld = $this->country_data[$top_cc]['tld'];
+         $tag = $this->Settings['tag_' . $top_cc];
+         $text='http://www.amazon.' . $tld . '/gp/product/'. $asin. '?ie=UTF8&tag=' . $tag .'&linkCode=as2&camp=1634&creative=6738&creativeASIN='. $asin;
+         return $text;
+      }
+
+      public function getURLs($asin)
+      {
+         $top_cc  = $this->get_country();
+         
+         $tld = $this->country_data[$top_cc]['tld'];
+         $tag = $this->Settings['tag_' . $top_cc];
+         $text[$top_cc]='http://www.amazon.' . $tld . '/gp/product/'. $asin. '?ie=UTF8&tag=' . $tag .'&linkCode=as2&camp=1634&creative=6738&creativeASIN='. $asin;
+         foreach ($this->country_data as $cc => $data) {
+            if ($cc != $top_cc) {
+               $tld = $data['tld'];
+               $tag = $this->Settings['tag_'.$cc];
+               $text[$cc]='http://www.amazon.' . $tld . '/gp/product/'. $asin. '?ie=UTF8&tag=' . $tag .'&linkCode=as2&camp=1634&creative=6738&creativeASIN='. $asin;
+            }
+         }
          return $text;
       }
 
       public function doQuery($request)
       {
-         return aws_signed_request($this->Settings['tld'], $request, $this->Settings['pub_key'], $this->Settings['priv_key']);
+         return aws_signed_request($this->country_data[$this->Settings['default_cc']]['tld'], $request, $this->Settings['pub_key'], $this->Settings['priv_key']);
       }
 
-      function makeLink($link_text)
+      function make_links($asin, $link_text)
       {
-         $text='<a href="' . $this->getURL() .'">' .$link_text. '</a>';
+         $URLs = $this->getURLs($asin);
+         $URL = array_shift($URLs);
+         if ($this->Settings['multi_cc']) {
+            $text='<a onMouseOut="al_link_out()" href="' . $URL .'" onMouseOver="al_link_in('. $this->multi_id . ',\'';
+            $js = '';
+            foreach ($URLs as $cc => $link) {
+               $js.="<a href='" . $link . "'><img src='". $this->URLRoot . "/" . $this->country_data[$cc]['flag']. "'></a>";
+            }
+            $text .= addslashes($js) .'\')">' . stripslashes($link_text). '</a>';
+            if ($this->multi_id == 0) {
+               $text .= '<span id="al_popup" onmouseover="al_div_in()" onmouseout="al_div_out()"></span>';
+               $this->done_div = True;
+            }
+            $this->multi_id++;
+         } else {
+            $text='<a href="' . $URL .'">' . stripslashes($link_text). '</a>';
+         }
          return $text;
       }
 
@@ -265,7 +424,14 @@ function amazon_get_link($args)
 {
   global $awlfw;
   $awlfw->parseArgs($args);       // Get the default settings
-  return $awlfw->getURL();        // Return a URL
+  return $awlfw->getURL($awlfw->Settings['asin']);        // Return a URL
+}
+
+function amazon_get_links($args)
+{
+  global $awlfw;
+  $awlfw->parseArgs($args);       // Get the default settings
+  return $awlfw->getURLs($awlfw->Settings['asin']);        // Return an array of URLs
 }
 
 function amazon_query($request)
