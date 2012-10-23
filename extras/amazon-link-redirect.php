7759a4000a95307f5a837a270673202057993941
paulstuttard <paulstuttard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 15:22:51 +0000

    Update Redirect Plugin
    git-svn-id: https://plugins.svn.wordpress.org/amazon-link/trunk@616336 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- extras/amazon-link-redirect.php
+++ extras/amazon-link-redirect.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Amazon Link Extra - Redirect
 Plugin URI: http://www.houseindorset.co.uk/plugins/amazon-link/
-Description: Adds the ability to redirect to any Amazon Link product using a URL of the format www.mydomain.com/go/0123456789/?args
+Description: Adds the ability to redirect to any Amazon Link product using a URL of the format www.mydomain.com/go/&lt;ASIN>/&lt;LINK TYPE S,R or A>/&lt;Domain ca,cn,de, etc.>/?args
 Version: 1.0
 Author: Paul Stuttard
 Author URI: http://www.houseindorset.co.uk
@@ -28,37 +28,97 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 
+/*
+ * The Redirect action function that is called when the Amazon Link plugin is Initialised
+ */
 function alx_redirect($settings, $al) {
+
+   $type_map = array ( 'A' => 'product', 'S' => 'search', 'R' => 'review');
+
    $uri = $_SERVER['REQUEST_URI'];
-   $match = preg_match( '!^/go[/?](([0-9a-z]*)/)?!', $uri, $args);
+   $match = preg_match( '!^/'.$settings['redirect_word'].'[/?](([0-9a-z]*)[/?])?((A|S|R)[/?])?((ca|cn|de|fr|es|jp|uk|us)[/?])?!', $uri, $args);
    if ( $match ) {
       $arg_position = strpos($uri,'?');
       if ($arg_position > 0) $parameters = substr($uri,$arg_position+1);
+      $home_cc = $settings['default_cc'];
 
-      $al->parseArgs('asin='.$args[2].'&'. $parameters);
+      $type = !empty($args[4]) ? $args[4] : 'A';
+      $type = $type_map[$type];
 
-      $settings   = $al->getSettings();
-      $local_info = $al->get_local_info();
-      $asin       = $settings['asin'][0];
-
-      if (isset($asin[$local_info['cc']])) {
-         // User Specified ASIN always use
-         $url_term = 'A-' . $asin[$local_info['cc']];
-      } else if ( ($settings['search_link']) ) {//&& (strstr($settings['search_text'], '%') == FALSE)) {
-         $settings['template_content'] = $settings['search_text'];
-         $search_text = $al->search->parse_template($settings);
-         $url_term = 'S-'.$search_text;
+      if (!empty($args[6])) {
+         $settings='default_cc='. $args[6].'&localise=0&';
       } else {
-         $url_term = 'A:' . $asin[$settings['default_cc']];
+         $settings ='';
       }
 
-      $url  = $al->getURL($url_term, $local_info['tld'], $local_info['tag']);
+      $al->parseArgs($settings.'asin='.$args[2].'&'. $parameters);
+      $settings   = $al->getSettings();
+      $settings['asin'] = $settings['asin'][0];
+      $settings['template_content'] = $settings['search_text'];
+      $settings['home_cc'] = $home_cc;
 
+      $url = $al->get_url('',$type, $settings['asin'], $al->search->parse_template($settings), $al->get_local_info($settings), $settings);
+//echo "<PRE>"; print_r($url); echo "</PRE>";
       wp_redirect($url, '302');
       die();
    }
 }
 
+/*
+ * Create a redirection style URL
+ */
+function alx_redirect_url($url, $type, $asin, $search, $local_info, $settings) {
+
+   /* Work out which ASIN to use */
+   if (isset($asin[$local_info['cc']])) {
+
+      // User Specified ASIN always use
+      $asin = $asin[$local_info['cc']].'/';
+   } else if ($settings['search_link'] && ($type == 'product')) {
+
+      // User wants search links for non-local domains
+      $type = 'search';
+      $asin = '';
+   } else {
+     
+      // Try using the default cc ASIN
+      $asin = $asin[$settings['home_cc']].'/';
+   }
+
+   // If not localised then force redirect function to send to specific locale.
+   if (!$settings['localise']) {
+      $cc = $local_info['cc'] . '/';
+   }
+
+   // If search links are enabled then pass the search text as an argument
+   if ($settings['search_link'] || ($type == 'search')) {
+      $search = '?search_text='. $search;
+   } else {
+      $search = '';
+   }
+
+   $type_map = array( /*'product' => 'A/',*/ 'search' => 'S/', 'review' => 'R/');
+   $text= get_option('home'). '/'. $settings['redirect_word']. '/'. $asin . $type_map[$type]. $cc . $search;
+   return $text;
+}
+
+/*
+ * Add the Redirect option to the Amazon Link Settings Page
+ */
+function alx_redirect_options ($options_list) {
+   $options_list['redirect_word'] = array ( 'Name' => __('Redirect Word', 'amazon-link'),
+                                            'Description' => __('The word that the redirect plugin looks for to indicate that it should try and link to Amazon (www.yourdomain.com/<REDIRECT WORD>/ASIN/?options)', 'amazon-link'),
+                                            'Type' => 'text',
+                                            'Default' => 'al',
+                                            'Class' => 'al_border');
+   return $options_list;
+}
+
+/*
+ * Install the Redirect option, filter and action
+ */
+add_filter('amazon_link_option_list', 'alx_redirect_options');
 add_action('amazon_link_init', 'alx_redirect',12,2);
+add_filter('amazon_link_url', 'alx_redirect_url', 10, 6);
 
 ?>
